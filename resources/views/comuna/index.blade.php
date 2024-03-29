bf057c780554f4f3c398d1a7cfb03f21de65d020
Jeison Manuyama <jeisonmanuyama@gmail.com>
2024-03-28 21:40:53 -0500

    Commune Add

--- resources/views/comuna/index.blade.php
+++ resources/views/comuna/index.blade.php
@@ -15,7 +15,7 @@
 <body> 
 <div class="container">
     <h1>Listado de Comunas</h1>
-    <a href="{{ route('comunas.create') }}" class="btn btn-success">Agregar</a>
+    <a href="{{ route('comunas.create')}}" class="btn btn-success">Agregar</a>
     <table class="table">
         <thead>
             <tr>
